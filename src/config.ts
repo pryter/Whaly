e2d497d14300023aaa5b3a9509ed8eec6c5fc61e
pryter <peterphongpak@gmail.com>
2022-11-03 23:13:02 +0700

    Increase disconnectTime && fix duplicated now playing embed bug

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { IntentsBitField } from "discord.js"
 
 export const config = {
-  disconnectTime: 10000,
+  disconnectTime: 30000,
   selfDestroyMessageLifeSpan: 5000,
   longLivedSDMLifeSpan: 10000,
   requiredIntents: [
--- src/whaly/events/manager/trackStart.ts
+++ src/whaly/events/manager/trackStart.ts
@@ -1,7 +1,7 @@
 import { Manager } from "erela.js"
 import { nowPlayingEmbed } from "@main/elements/embeds/nowPlaying"
 import { getChannel } from "@utils/cache"
-import { Client, TextChannel } from "discord.js"
+import { Client, Message, MessageCreateOptions, MessageEditOptions, TextChannel } from "discord.js"
 import { controllerStrip } from "@main/elements/buttons/controllerStrip"
 import { log, warn } from "@utils/logger"
 
@@ -9,16 +9,22 @@ export const registerTrackStartEvent = (manager: Manager, client: Client) => {
   manager.on("trackStart", async (player, track, payload) => {
     const embed = nowPlayingEmbed(track)
     const textChannel = <TextChannel>getChannel(client, player.textChannel)
+    const content: MessageCreateOptions = {
+      embeds: [embed],
+      //@ts-ignore
+      components: [controllerStrip(player)],
+    }
 
     log(`player | Playing ${track.title} @ ${player.guild}`)
 
-    const nowPlayingMessage = await textChannel
-      .send({
-        embeds: [embed],
-        //@ts-ignore
-        components: [controllerStrip(player)],
-      })
-      .catch(warn)
+    const nowPlaying: Message | null | undefined = player.get("nowPlaying")
+
+    if (nowPlaying) {
+      nowPlaying.edit(content)
+      return
+    }
+
+    const nowPlayingMessage = await textChannel.send(content).catch(warn)
     player.set("nowPlaying", nowPlayingMessage)
   })
 }

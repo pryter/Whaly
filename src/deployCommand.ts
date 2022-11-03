637cc13312a138296ebe27568c2b03bd0ff9d1e7
pryter <peterphongpak@gmail.com>
2022-11-03 22:31:14 +0700

    set presence

--- src/deployCommand.ts
+++ src/deployCommand.ts
@@ -1,4 +1,3 @@
 import { commandDeploy } from "@utils/commandDeploy"
-import "module-alias/register"
 
 commandDeploy()
--- src/whaly/events/client/readyEvent.ts
+++ src/whaly/events/client/readyEvent.ts
@@ -1,10 +1,20 @@
-import { Client } from "discord.js"
+import { ActivityType, Client } from "discord.js"
 import { info } from "@utils/logger"
 import { Manager } from "erela.js"
 
 export const registerReadyEvent = (client: Client, manager: Manager) => {
   client.on("ready", () => {
     manager.init(process.env.CLIENT_ID)
+    client.user?.setPresence({
+      status: "online",
+      activities: [
+        {
+          name: "With Your 💖🫶🏻 💞",
+          type: ActivityType.Playing,
+        },
+      ],
+    })
+
     info("client | Successfully logged in")
   })
 }

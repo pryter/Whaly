4fc60f0ce4cdb9f2e3ede1632abec0b0865ccde2
pryter <peterphongpak@gmail.com>
2022-11-08 23:32:52 +0700

    Update config

--- src/whaly/elements/embeds/resume.ts
+++ src/whaly/elements/embeds/resume.ts
@@ -5,6 +5,6 @@ import { Track, UnresolvedTrack } from "erela.js"
 export const resumeEmbed = (client: Client, resumedTrack: Track | UnresolvedTrack) => {
   return new EmbedBuilder()
     .setAuthor({ name: "Resumed!", iconURL: runningCat })
-    .setDescription(`Playing  [${resumedTrack.title}](${resumedTrack.uri}) by \`${resumedTrack.requester}\``)
+    .setDescription(`Playing  [${resumedTrack.title}](${resumedTrack.uri}) by ${resumedTrack.requester}`)
     .setFooter({ text: `The current song has been resumed.` })
 }

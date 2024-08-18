d7050c071d45bad66e59d58ed76643db2e4ac851
peter <peterphongpak@gmail.com>
2024-08-18 12:50:24 +0000

    fix: script functions error

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,6 +11,14 @@ createApplicationConfig() {
   sed -e "s/YT_VER/${YT_PLUGIN_VERSION}/g" -e "s/_PASSWORD_/$answer/g" $TEMPLATE > application.yml
 }
 
+downloadLavalink () {
+  echo "Downloading compatible lavalink (${LAVALINK_VERSION})"
+  mkdir lavalink
+  wget -O lavalink/Lavalink.jar "https://github.com/lavalink-devs/Lavalink/releases/download/${LAVALINK_VERSION}/Lavalink.jar"
+  echo "${LAVALINK_VERSION}" > lavalink/version.txt
+  chmod +x lavalink/Lavalink.jar
+}
+
 if [ -f "lavalink/Lavalink.jar" ]
 then
     if [ "${LAVALINK_VERSION}" != "$(cat lavalink/version.txt)" ] ;then
@@ -26,11 +34,3 @@ createApplicationConfig
 
 echo "Successfully setup lavalink deps."
 echo "Try yarn lavalink to start the server."
-
-downloadLavalink () {
-  echo "Downloading compatible lavalink (${LAVALINK_VERSION})"
-  mkdir lavalink
-  wget -O lavalink/Lavalink.jar "https://github.com/lavalink-devs/Lavalink/releases/download/${LAVALINK_VERSION}/Lavalink.jar"
-  echo "${LAVALINK_VERSION}" > lavalink/version.txt
-  chmod +x lavalink/Lavalink.jar
-}
